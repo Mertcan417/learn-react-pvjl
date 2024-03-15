629df75799530bd9af8daaf32c95fa5423f3c354
mert <boymerti@gmail.com>
2024-03-15 18:16:43 +0100

    remove unnecessary files in opdrachten_quick_start

--- Quick_Start/opdrachten_quick_start/src/pages/index.js
+++ Quick_Start/opdrachten_quick_start/src/pages/index.js
@@ -1,13 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
 import ExampleComponent from "./ExampleComponent.js";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
       <ExampleComponent />
-    </div>
   );
 }
