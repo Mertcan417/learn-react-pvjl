8aafa788299ecd80481c3a0fe396d96118f7059d
mert <boymerti@gmail.com>
2024-03-15 22:06:07 +0100

    add squares for making tictactoe in Quick start module

--- Quick_Start/opdrachten_quick_start/src/pages/index.js
+++ Quick_Start/opdrachten_quick_start/src/pages/index.js
@@ -1,9 +1,18 @@
 import ExampleComponent from "./ExampleComponent.js";
 import styles from "../styles/index.module.css" 
 export default function Home() {
+
+  const randomUser = {name : "Vito Scalletta", age: 23, country: "Italy"} 
   return (
+    
     <div className={styles.home}>
       <ExampleComponent />
+      <h1>random userdata</h1>
+      <ul>
+      <li>{randomUser.name}</li>
+      <li>{randomUser.age}</li>
+      <li>{randomUser.country}</li>
+      </ul>
       <button> I'm a button</button>
     </div>
 
