4d9f34d4521c29cb00e86fb87f493070e7995df5
mert <boymerti@gmail.com>
2024-03-15 23:32:26 +0100

    add square components in board component

--- Quick_Start/opdrachten_quick_start/src/pages/tictactoe.js
+++ Quick_Start/opdrachten_quick_start/src/pages/tictactoe.js
@@ -10,10 +10,19 @@ function Board() {
   return (
     <>
       <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
       </div>
       <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
       </div>
       <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
       </div>
     </>
   );
