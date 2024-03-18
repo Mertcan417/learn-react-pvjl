0ae6ce5498c9bcb84d367dc877b81b3fe62a9096
mert <boymerti@gmail.com>
2024-03-18 17:55:15 +0100

    add solution for exercises for module Your first component

---
+++ Describing_The_UI/your_first_component/src/pages/Exercise2.js
@@ -0,0 +1,5 @@
+// Fix the return statement
+
+export default function Profile() {
+  return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+}
---
+++ Describing_The_UI/your_first_component/src/pages/Exercise4.js
@@ -0,0 +1,5 @@
+//Your own component!
+
+export default function MyOwnComponent() {
+  return <div>Hello world, this is my first made component!</div>;
+}
