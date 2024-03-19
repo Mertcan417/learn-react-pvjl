7a0faafc495d89cd79ee3d2c695934007d6b5834
mert <boymerti@gmail.com>
2024-03-19 18:10:20 +0100

    add solution for third exercise for module Javascript in jsx with curly braces

--- Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/index.js
+++ Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/index.js
@@ -1,11 +1,13 @@
 import TodoList1 from "./exercise1";
 import TodoList2 from "./exercise2";
+import TodoList3 from "./exercise3";
 
 export default function Home() {
   return (
     <>
       <TodoList1></TodoList1>
       <TodoList2></TodoList2>
+      <TodoList3></TodoList3>
     </>
   );
 }
