789244ed63e3d2b99666fab97099ee06cbfec621
mert <boymerti@gmail.com>
2024-03-19 18:05:48 +0100

    add solution for second exercise for module Javascript in jsx with curly braces

--- Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/exercise2.js
+++ Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/exercise2.js
@@ -1,21 +1,22 @@
-//Fix the mistake.
-//This code crashes with an error saying Objects are not valid as a React child:
+// Extract information into an object 
+// Extract the image URL into the person object.
 
 const person = {
     name: 'Gregorio Y. Zara',
     theme: {
       backgroundColor: 'black',
       color: 'pink'
-    }
+    },
+    image: "https://i.imgur.com/7vQD0fPs.jpg"
   };
   
-  export default function TodoList1() {
+  export default function TodoList2() {
     return (
       <div style={person.theme}>
         <h1>{person.name}'s Todos</h1>
         <img
           className="avatar"
-          src="https://i.imgur.com/7vQD0fPs.jpg"
+          src={person.image}
           alt="Gregorio Y. Zara"
         />
         <ul>
--- Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/index.js
+++ Describing_The_UI/javascript_in_jsx_with_curly_braces/src/pages/index.js
@@ -1,11 +1,11 @@
-import TodoList from "./exercise1";
-
+import TodoList1 from "./exercise1";
+import TodoList2 from "./exercise2";
 
 export default function Home() {
   return (
     <>
-    <TodoList>
-    </TodoList>
+      <TodoList1></TodoList1>
+      <TodoList2></TodoList2>
     </>
   );
 }
