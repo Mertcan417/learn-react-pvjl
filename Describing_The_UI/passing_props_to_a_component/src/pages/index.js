20379bdb32a099a485f5d697332bc56ff65565ad
mert <boymerti@gmail.com>
2024-03-22 00:43:39 +0100

    add solution for second  exercise for module passing props to components

--- Describing_The_UI/passing_props_to_a_component/src/pages/index.js
+++ Describing_The_UI/passing_props_to_a_component/src/pages/index.js
@@ -1,8 +1,10 @@
 import Gallery from "./exercise1";
+import Profile from "./exercise2";
 export default function Home() {
   return (
     <>
       <Gallery></Gallery>
+      <Profile></Profile>
     </>
   );
 }
