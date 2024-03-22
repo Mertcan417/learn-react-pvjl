1e776fcfb84eee87027a7c65f3aa436e91ceabde
mert <boymerti@gmail.com>
2024-03-22 16:15:04 +0100

    add exercises for conditional rendering module

---
+++ Describing_The_UI/conditional_rendering/src/pages/index.js
@@ -0,0 +1,13 @@
+import PackingList1 from "./Exercise1";
+import PackingList2 from "./exercise2";
+import DrinkList from "./exercise3";
+
+export default function Home() {
+  return (
+    <>
+      <PackingList1></PackingList1>
+      <PackingList2></PackingList2>
+      <DrinkList></DrinkList>
+    </>
+  );
+}
