1ab3b970deae2fff20db5294a4287ce28a708ae4
mert <boymerti@gmail.com>
2024-04-02 17:13:25 +0200

    add solution for fourth exercise for module preserving and resetting state

--- Managing_State/preserving_and_resetting_state/src/pages/exercise4.js
+++ Managing_State/preserving_and_resetting_state/src/pages/exercise4.js
@@ -1,2 +1,63 @@
 /**Challenge 4 of 5: Clear an image while it’s loading 
 When you press “Next”, the browser starts loading the next image. However, because it’s displayed in the same <img> tag, by default you would still see the previous image until the next one loads. This may be undesirable if it’s important for the text to always match the image. Change it so that the moment you press “Next”, the previous image immediately clears. */
+
+import { useState } from 'react';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const hasNext = index < images.length - 1;
+
+  function handleClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  let image = images[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h3>
+        Image {index + 1} of {images.length}
+      </h3>
+      <img src={image.src} key={image.key} />
+      <p>
+        {image.place}
+      </p>
+    </>
+  );
+}
+
+let images = [{
+  key: "Penang",
+  place: 'Penang, Malaysia',
+  src: 'https://i.imgur.com/FJeJR8M.jpg'
+}, {
+  key: "Lisbon",
+  place: 'Lisbon, Portugal',
+  src: 'https://i.imgur.com/dB2LRbj.jpg'
+}, {
+  key: "Bilbao",
+  place: 'Bilbao, Spain',
+  src: 'https://i.imgur.com/z08o2TS.jpg'
+}, {
+  key:"Vaparaiso",
+  place: 'Valparaíso, Chile',
+  src: 'https://i.imgur.com/Y3utgTi.jpg'
+}, {
+  key: "Schwyz",
+  place: 'Schwyz, Switzerland',
+  src: 'https://i.imgur.com/JBbMpWY.jpg'
+}, {
+  key: "Prague",
+  place: 'Prague, Czechia',
+  src: 'https://i.imgur.com/QwUKKmF.jpg'
+}, {
+  key: "Ljubljana",
+  place: 'Ljubljana, Slovenia',
+  src: 'https://i.imgur.com/3aIiwfm.jpg'
+}];
