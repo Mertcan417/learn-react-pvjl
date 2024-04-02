be7efd1208af055b0c40f5427bd3ef4bf7556fe6
mert <boymerti@gmail.com>
2024-04-02 17:14:15 +0200

    add solutions for exercises in index.js file for module preserving and resetting state

--- Managing_State/preserving_and_resetting_state/src/pages/index.js
+++ Managing_State/preserving_and_resetting_state/src/pages/index.js
@@ -1,12 +1,17 @@
 import App1 from "./exercise1";
 import App2 from "./exercise2";
+import ContactManager from "./exercise3";
+import Gallery from "./exercise4";
+import ContactList from "./exercise5";
 
 export default function Home() {
   return (
     <div>
       <App1></App1>
       <App2></App2>
-      
+      <ContactManager></ContactManager>
+      <Gallery></Gallery>
+      <ContactList></ContactList>
     </div>
   );
 }
