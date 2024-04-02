55a353419a3fc41fee6c59f4dcc8054bf6c7932a
mert <boymerti@gmail.com>
2024-04-02 16:28:56 +0200

    add solutiion for second exercise for module preserving and resetting state

--- Managing_State/preserving_and_resetting_state/src/pages/exercise2.js
+++ Managing_State/preserving_and_resetting_state/src/pages/exercise2.js
@@ -2,3 +2,51 @@
 This form lets you enter first and last name. It also has a checkbox controlling which field goes first. When you tick the checkbox, the “Last name” field will appear before the “First name” field.
 
 It almost works, but there is a bug. If you fill in the “First name” input and tick the checkbox, the text will stay in the first input (which is now “Last name”). Fix it so that the input text also moves when you reverse the order. */
+
+import { useState } from 'react';
+
+export default function App2() {
+  const [reverse, setReverse] = useState(false);
+  let checkbox = (
+    <label>
+      <input
+        type="checkbox"
+        checked={reverse}
+        onChange={e => setReverse(e.target.checked)}
+      />
+      Reverse order
+    </label>
+  );
+  if (reverse) {
+    return (
+      <>
+        <Field key="last" label="Last name" /> 
+        <Field key="first" label="First name" />
+        {checkbox}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Field key="first" label="First name" /> 
+        <Field key="last" label="Last name" />
+        {checkbox}
+      </>
+    );    
+  }
+}
+
+function Field({ label }) {
+  const [text, setText] = useState('');
+  return (
+    <label>
+      {label}:{' '}
+      <input
+        type="text"
+        value={text}
+        placeholder={label}
+        onChange={e => setText(e.target.value)}
+      />
+    </label>
+  );
+}
--- Managing_State/preserving_and_resetting_state/src/pages/index.js
+++ Managing_State/preserving_and_resetting_state/src/pages/index.js
@@ -1,9 +1,12 @@
 import App1 from "./exercise1";
+import App2 from "./exercise2";
 
 export default function Home() {
   return (
     <div>
       <App1></App1>
+      <App2></App2>
+      
     </div>
   );
 }
